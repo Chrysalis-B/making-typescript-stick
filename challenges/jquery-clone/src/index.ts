4472ae10f234087a3a548578e9e1ab1c699f1a53
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-06-13 16:53:13 +0200

    Finish jquery clone exercise

--- challenges/jquery-clone/src/index.ts
+++ challenges/jquery-clone/src/index.ts
@@ -1,12 +1,47 @@
 import fetch from "node-fetch";
 
-function $(selector: string): any {
-  return {} as any;
+class SelectorResult {
+  #elements;
+  on<K extends keyof HTMLElementEventMap>(eventName: K,
+    cb: (event: HTMLElementEventMap[K]) => void) {
+    this.#elements.forEach(elem => {
+      const htmlElem = elem as HTMLElement;
+      htmlElem.addEventListener(eventName, cb)
+    });
+  }
+  show() {
+    this.#elements.forEach(elem => {
+      const htmlElem = elem as HTMLElement;
+      htmlElem.style.visibility = 'visible';
+    });
+  }
+  hide() {
+    this.#elements.forEach(elem => {
+      const htmlElem = elem as HTMLElement;
+      htmlElem.style.visibility = 'hidden';
+    });
+  }
+  html(contents: string) {
+    this.#elements.forEach(elem => {
+      elem.innerHTML = contents;
+    });
+  }
+  constructor(elements: NodeListOf<Element>) {
+    this.#elements = elements;
+  }
+}
+
+function $(selector: string) {
+  return new SelectorResult(document.querySelectorAll(selector));
 }
 
 namespace $ {
-  export function ajax(...args: any[]): any {
-    return {} as any;
+  export function ajax(
+    { url, successCb }:
+      { url: string, successCb: (data: any) => void }): any {
+    return fetch(url)
+      .then(resp => resp.json())
+      .then(successCb);
   }
 }
 
